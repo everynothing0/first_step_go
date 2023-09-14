113472296203b05c087ac8fb3321fc3cfde79b0e
Constance De Voogd <cde-voog@c2r12p1.42lehavre.fr>
2023-09-14 15:43:25 +0200

    look the name and you know

---
+++ operators.go
@@ -0,0 +1,18 @@
+// operators lol
+
+package main
+
+import ("fmt")
+
+func main() {
+	
+	var a = 15 + 25
+	fmt.Println(a)
+
+	var (
+	sum1 = 100 + 50
+	sum2 = sum1 + 250
+	sum3 = sum2 + sum2
+	)
+	fmt.Println(sum3)
+}
