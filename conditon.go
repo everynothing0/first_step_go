7c76dc4d2e1ee0f4d050b2dd682c9f72be872a87
Constance De Voogd <cde-voog@c1r7p5.42lehavre.fr>
2023-09-18 17:10:36 +0200

    conditions base

--- conditon.go
+++ conditon.go
@@ -4,11 +4,23 @@ package main
 
 import ("fmt")
 
-func main() {
+/*func main() {
 	time := 20
 	if (time < 18) {
 		fmt.Println("Good day.")
 	} else {
 		fmt.Println("Good evening.")
+	}
+} */
+
+func main() {
+	a := 14
+	b := 14
+	if a < b {
+		fmt.Println("a is less than b.")
+	} else if a > b {
+		fmt.Println("a is more than b.")
+	} else {
+		fmt.Println("a and b are equal.")
 	}
 }
