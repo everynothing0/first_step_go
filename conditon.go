473bd4ae4159815763ebdc3b9affb8e26204cf34
Constance De Voogd <cde-voog@c1r7p5.42lehavre.fr>
2023-09-18 12:38:33 +0200

    condition

---
+++ conditon.go
@@ -0,0 +1,14 @@
+// don't forget de brackets
+
+package main
+
+import ("fmt")
+
+func main() {
+	time := 20
+	if (time < 18) {
+		fmt.Println("Good day.")
+	} else {
+		fmt.Println("Good evening.")
+	}
+}
