64a6b6adea157c8cd3baede38ba5ca54770e6d42
Constance De Voogd <cde-voog@c2r12p1.42lehavre.fr>
2023-09-19 14:02:52 +0200

    for loop

--- for_loop.go
+++ for_loop.go
@@ -11,4 +11,12 @@ func main() {
 		}
 	fmt.Println(i)
 	}
+
+	adj := [2]string{"big", "tasty"}
+	fruits := [3]string{"apple", "orange", "banana"}
+	for i := 0; i < len(adj); i++ {
+		for j := 0; j <  len(fruits); j++ {
+			fmt.Println(adj[i],fruits[j])
+		}
+	}
 }
