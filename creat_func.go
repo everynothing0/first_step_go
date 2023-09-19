93e6a270978cda0e2fc5a99baeebe75196ed558e
Constance De Voogd <cde-voog@c2r12p1.42lehavre.fr>
2023-09-19 14:31:29 +0200

    create, and call

--- creat_func.go
+++ creat_func.go
@@ -9,8 +9,15 @@ func message() {
 	fmt.Println("I just got executed!")
 }
 
+func fam_name(fname string, age int) {
+	fmt.Println("Hello", fname, "Ref", age, "year old")
+}
+
 func main() {
 	message()
+	fam_name("Ju", 3)
 	message()
+	fam_name("corr", 5)
+	fam_name("next", 31)
 	message()
 }
