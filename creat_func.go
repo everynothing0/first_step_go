48983ff3c2bb1a20304dd6a1eb077a62f370c0cf
Constance De Voogd <cde-voog@c2r12p1.42lehavre.fr>
2023-09-19 14:41:24 +0200

    creatw func and more

--- creat_func.go
+++ creat_func.go
@@ -13,11 +13,25 @@ func fam_name(fname string, age int) {
 	fmt.Println("Hello", fname, "Ref", age, "year old")
 }
 
+func null() {
+	fmt.Printf("\n")
+}
+
+func funct(x int, y int) int {
+	return x + y
+}
+
 func main() {
+
+	null()
 	message()
 	fam_name("Ju", 3)
 	message()
+	null()
+	fmt.Println(funct(1, 2))
+	null()
 	fam_name("corr", 5)
 	fam_name("next", 31)
 	message()
+	null()
 }
